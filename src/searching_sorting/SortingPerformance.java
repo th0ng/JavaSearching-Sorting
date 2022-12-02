e2a5d58b0d3f97d4ab11ce2267fd787f8b321be2
Thongzxje <thonghoangdanh@gmail.com>
2022-12-02 14:55:18 +0200

    quick sort

--- src/searching_sorting/SortingPerformance.java
+++ src/searching_sorting/SortingPerformance.java
@@ -1,26 +1,26 @@
 package searching_sorting;
+
 import java.util.Arrays;
 import java.util.Random;
 
 public class SortingPerformance {
-	//Define the arrays gonna used during sorting
+	// Define the arrays gonna used during sorting
 	private static final int n = 1000;
 	private static int[] array1 = new int[n];
-	private static int[] array2 = new int[n*2];
-	private static int[] array3 = new int[n*3];
-	private static int[] array4 = new int[n*4];
-	private static int[] array5 = new int[n*5];
-	private static int[] array6 = new int[n*6];
-	private static int[] array7 = new int[n*7];
-	private static int[] array8 = new int[n*8];
-	private static int[] array9 = new int[n*9];
-	private static int[] array10 = new int[n*10];
+	private static int[] array2 = new int[n * 2];
+	private static int[] array3 = new int[n * 3];
+	private static int[] array4 = new int[n * 4];
+	private static int[] array5 = new int[n * 5];
+	private static int[] array6 = new int[n * 6];
+	private static int[] array7 = new int[n * 7];
+	private static int[] array8 = new int[n * 8];
+	private static int[] array9 = new int[n * 9];
+	private static int[] array10 = new int[n * 10];
 
-	//Array of 10 arrays
-	private static int[][] array = {array1, array2, array3, array4, array5, array6, array7, array8, array9, array10};
+	// Array of 10 arrays
+	private static int[][] array = { array1, array2, array3, array4, array5, array6, array7, array8, array9, array10 };
 
-
-	//Method to create array with random integer in it
+	// Method to create array with random integer in it
 	static void createRandomArray(int[] array) {
 		Random rd = new Random();
 		for (int i = 0; i < array.length; i++) {
@@ -34,29 +34,30 @@ public class SortingPerformance {
 	static int[] selectionSort(int[] array) {
 		int n = array.length;
 		int comparisonCount = 0;
-		//Starting the timer
+		// Starting the timer
 		long start = System.currentTimeMillis();
-		for (int i = 0; i < n -1; i++) {
+		for (int i = 0; i < n - 1; i++) {
 			int min_index = i;
-			//Find the minimum element in the unsorted array
-			for (int j = i+1; j < n; j++) {
-				//Calculate the comparison being taken and added to the total
+			// Find the minimum element in the unsorted array
+			for (int j = i + 1; j < n; j++) {
+				// Calculate the comparison being taken and added to the total
 				comparisonCount++;
 
 				if (array[j] < array[min_index])
 					min_index = j;
 
-				//Swap the minimum element with the first element
+				// Swap the minimum element with the first element
 				int temp = array[min_index];
 				array[min_index] = array[i];
 				array[i] = temp;
 			}
 		}
-		//Stopping the timer
+		// Stopping the timer
 		long end = System.currentTimeMillis();
 
-		return new int[] {(int) (end - start), comparisonCount};
+		return new int[] { (int) (end - start), comparisonCount };
 	}
+
 	// Selection Sort interface
 	static int[][] SelectionSort() {
 		for (int[] ints : array) {
@@ -70,17 +71,17 @@ public class SortingPerformance {
 		return result;
 	}
 
-	//Insertion sorting function
+	// Insertion sorting function
 	static int[] insertionSort(int[] array) {
 		int length = array.length;
 		int comparisonCount = 0;
-		//Start the timer
+		// Start the timer
 		long start = System.currentTimeMillis();
-		//Loop
+		// Loop
 		for (int index = 1; index < length; index++) {
 			int key = array[index];
 			int position = index;
-			//shift larger values to the right
+			// shift larger values to the right
 			while (position > 0 && array[position - 1] > key) {
 				array[position] = array[position - 1];
 				position--;
@@ -88,16 +89,15 @@ public class SortingPerformance {
 			}
 			array[position] = key;
 		}
-		//Stop the timer
+		// Stop the timer
 		long end = System.currentTimeMillis();
-		return new int[] {(int) (end-start), comparisonCount};
+		return new int[] { (int) (end - start), comparisonCount };
 	}
 
-	//Insertion sorting interface
+	// Insertion sorting interface
 	static int[][] InsertionSort() {
-		for (int[] ints : array) {
+		for (int[] ints : array)
 			createRandomArray(ints);
-		}
 		int[][] result = new int[10][];
 		for (int i = 0; i < array.length; i++) {
 			result[i] = insertionSort(array[i]);
@@ -105,13 +105,129 @@ public class SortingPerformance {
 		return result;
 	}
 
+	// O(n*log(n)) sorting: merge sort and quick sort
 
-	//O(n*log(n)) sorting: merge sort and quick sort
+	// Recursive merge sorting function
+	static int mergeSort(int[] array) {
+		int comparisonCount = 0;
+		int n = array.length;
+		if (n < 2)
+			return 0;
+		int mid = n / 2;
+		int[] left = new int[mid];
+		int[] right = new int[n - mid];
 
-	//Merge sorting interface
+		for (int i = 0; i < mid; i++) {
+			left[i] = array[i];
+
+		}
+		for (int i = mid; i < n; i++)
+			right[i - mid] = array[i];
+
+		// Recursive call on each half
+		comparisonCount = mergeSort(left) + mergeSort(right);
+
+		// Two iterators for traversing the two halves
+		int i = 0, j = 0;
+		// Iterator for the main list
+		int k = 0;
+
+		while (i < left.length && j < right.length) {
+			comparisonCount++;
+			if (left[i] <= right[j]) {
+				// The value from the left half has been used
+				array[k] = left[i];
+				// Move the iterator forward
+				i++;
+			} else {
+				array[k] = right[j];
+				j++;
+			}
+			// Move to the next slot
+			k++;
+		}
+		// For all the remaining value
+		while (i < left.length) {
+			array[k] = left[i];
+			i++;
+			k++;
+		}
+		while (j < right.length) {
+			array[k] = right[j];
+			j++;
+			k++;
+		}
+
+		return comparisonCount;
+	}
+
+	// Merge sorting interface
+	static int[][] MergeSort() {
+		for (int[] ints : array)
+			createRandomArray(ints);
+		int[][] result = new int[10][];
+		for (int i = 0; i < array.length; i++) {
+			long start = System.currentTimeMillis();
+			int counter = mergeSort(array[i]);
+			long end = System.currentTimeMillis();
+			result[i] = new int[] { (int) (end - start), counter };
+		}
+		return result;
+	}
+
+	// Quick sorting
+	// Function
+	static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	static int partition(int[] array, int low, int high) {
+		int pivot = array[high];
+		//index of smaller element
+		//and indicates the right position of pivot found so far
+		int i = (low -1);
+
+		for (int j = low; j <= high; j++) {
+			//if current element is smaller than the pivot
+			if (array[j] < pivot) {
+				//increment index of smaller element
+				i++;
+				swap(array, i, j);
+			}
+		}
+		swap(array, i+1, high);
+		return (i+1);
+	}
+
+	static void quickSort(int[] array, int low, int high) {
+		if (low < high) {
+			int pi = partition(array, low, high);
+
+			quickSort(array, low, pi -1);
+			quickSort(array, pi + 1, high);
+		}
+	}
+
+	static int[] QuickSort() {
+		for (int[] ints : array)
+			createRandomArray(ints);
+		int[] result = new int[10];
+		for (int i = 0; i < array.length; i++) {
+			long start = System.currentTimeMillis();
+			quickSort(array[i], 0, array[i].length -1);
+			long end = System.currentTimeMillis();
+			result[i] = (int) (end - start);
+		}
+		return result;
+	}
 
 
 	public static void SortingPerformanceToString() {
+
+		System.out.println(Arrays.deepToString(MergeSort()));
 		System.out.println(Arrays.deepToString(InsertionSort()));
+		System.out.println(Arrays.toString(QuickSort()));
 	}
- }
+}
